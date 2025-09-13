63660d4eb05d5b5e29467ce1cb1a228716023532
league <league@imac-34.attlocal.net>
2025-09-13 14:26:51 -0700

    I worked on Cell and WorldPanel.

--- src/_06_Conways_Game_of_Life/WorldPanel.java
+++ src/_06_Conways_Game_of_Life/WorldPanel.java
@@ -20,6 +20,8 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 	private Timer timer;
 
 	// 1. Create a 2D array of Cells. Do not initialize it.
+	
+	Cell cells[][];
 
 	public WorldPanel(int w, int h, int cpr) {
 		setPreferredSize(new Dimension(w, h));
@@ -28,19 +30,28 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
 		this.cellsPerRow = cpr;
 
 		// 2. Calculate the cell size.
-
+int cellSize = cells.length;
 		// 3a. Initialize the cell array to the appropriate size.
-
+cells = new Cell[50][50];
 		// 3b. Iterate through the array and initialize each cell.
 		// Don't forget to consider the cell's dimensions when
 		// passing in the location.
-
+for (int i =0; i<cellSize; i++) {
+	for (int j =0; j<cells[i].length; j++) {
+		cells[i][j] = new Cell(i,j, cellSize);
+	}
+}
 	}
 
 	public void randomizeCells() {
 		// 4. Iterate through each cell and randomly set each
 		// cell's isAlive memeber to true or false
-
+		
+for (int i =0; i<cellSize; i++) {
+	for (int j =0; j<cells[i].length; j++) {
+		
+	}
+}
 		repaint();
 	}
 
